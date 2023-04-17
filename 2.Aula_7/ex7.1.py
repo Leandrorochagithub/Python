2c372dc1be8eced5788c1503c687ab2c22561561
LEANDRO ROCHA <leandrodarochaferreira@gmail.com>
2023-04-17 09:58:52 -0300

    Aula 7, exercícios

---
+++ 2.Aula_7/ex7.1.py
@@ -0,0 +1,8 @@
+#n=int(input('Digite um número: '))
+#antesn= n-1
+#depoisn= n+1
+
+#print('Seu némro é {}, seu antecessor é {} e seu sucessor é {}.'.format(n, antesn, depoisn))
+
+n=int(input('Digite um número: '))
+print('Seu némro é {}, seu antecessor é {} e seu sucessor é {}.'.format(n, (n-1), (n+1)))
---
+++ 2.Aula_7/ex7.3.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite a primeira nota: '))
+n2=int(input('Digite a segunda nota: '))
+média=(n1+n2)/2
+print('A média de {} + {} é {}.'.format(n1, n2, média))
