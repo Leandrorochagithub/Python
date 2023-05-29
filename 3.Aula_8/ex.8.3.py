0e43a5e057d5a2f5c9735c00ec6185065699cfbc
LEANDRO ROCHA <leandrodarochaferreira@gmail.com>
2023-05-29 16:57:14 -0300

    Aula 8

---
+++ 3.Aula_8/ex.8.3.py
@@ -0,0 +1,3 @@
+real= float(input('Quanto voce tem na carteira? R$ '))
+dolar= real / 5.05
+print('Com R$ {} você pode comprar USS {:.2f} dólares!'.format(real,dolar))
---
+++ 3.Aula_8/ex.8.4.py
@@ -0,0 +1,3 @@
+valor=float(input('Digite o valor do produto: R$ '))
+desconto= valor - (valor*0.05)
+print('R$ {} com 5% de desconto é igual a R$ {}.'.format(valor,desconto))
